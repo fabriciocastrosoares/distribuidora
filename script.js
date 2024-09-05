3db67ce9382fe632c1c60c91787ac6d711a8c5d2
fabriciocastrosoares <fabriciosoares27@gmail.com>
2024-09-05 14:59:07 -0300

    mais java-script implementados

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-function selecionar(seletor){
+function selecionarCarne(selecionarThis){
 
   //pegar o botão selecionado anteriormente
-  const selecionadoAnterior = document.querySelector(".borda");
+  const selecionadoAnterior = document.querySelector(".carnes .borda");
 
   //antes de remover a class selecionado, verificar se o elemento não é null
   if (selecionadoAnterior !== null) {
 
   //remover a classe selecionado desse botão
   selecionadoAnterior.classList.remove("borda");
-}
-  //pegar o botão clicado 
-  const selecionar =document.querySelector(seletor);
-
+  }
   //adicionar a classe selecionado ao botão
-  selecionar.classList.add("borda");
+  selecionarThis.classList.add("borda");
+}
+
+
+function selecionarBebida(selecionarThis){
+
+  const selecionadoAnterior = document.querySelector(".bebidas .borda");
+
+  if (selecionadoAnterior !== null){
+    selecionadoAnterior.classList.remove("borda");
+  }
+
+  selecionarThis.classList.add("borda");
+}
+
+
+function selecionarSobremesa(selecionarThis){
+
+  const selecionadoAnterior = document.querySelector(".sobremesas .borda");
+
+  if (selecionadoAnterior !== null){
+    selecionadoAnterior.classList.remove("borda");
+  }
+
+  selecionarThis.classList.add("borda");
 }
